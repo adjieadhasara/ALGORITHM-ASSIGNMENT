6b83ac7100f9cd6b5d66008a40a831fe6c73097e
adjie__23 <adjie.sara20@gmail.com>
2021-11-09 20:56:22 +0800

    algorithm test

---
+++ javaScriptTest.js
@@ -0,0 +1,18 @@
+let math = (1, 2, 3, 4);
+
+function Mathematic() {
+  if (math === true) {
+    return alert("its true");
+  } else {
+    alert("its False");
+  }
+}
+
+function fullName() {
+  let name = "derr nugroho";
+  if (fullName === name) {
+    return name[0], "satu huruf depan";
+  } else {
+    name[10], "satu huruf belakang ";
+  }
+}
